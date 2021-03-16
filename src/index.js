3e2fed90df937ccd5880e6be3bf8e99ee5686bcd
AnwarHossainSR <mahedianwar@gmail.com>
2021-03-16 20:27:10 +0600

    JSX Attributes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom'
 
-const fname = 'Mahedi';
-const lname = 'Hasan';
-var today = new Date(),
-date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate(),
-time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-ReactDom.render(
-        <>
-          <h1>My first name is {fname} and last name is {lname}</h1>
-          <p>Today's date is : {date}</p>
-          <p>and time is : {time}</p>
-        </>,document.getElementById('root')
-        );
+const name = 'Mahedi Hasan'
+ReactDOM.render(
+  <h1 contentEditable="true">My name is {name}</h1>,
+  document.getElementById('root')
+);
