642ce27a3899df291a3e3be38b975a1617338260
AnwarHossainSR <mahedianwar@gmail.com>
2021-03-16 12:34:41 +0600

    React Fragement

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 ReactDom.render(
-      [
+      <React.Fragment>
         <h2>Hello Mahedi !!</h2>,
         <p>How are you?</p>,
         <p>Yah i am fine and you?</p>
-      ],
+      </React.Fragment>,
       document.getElementById('root')
       );
