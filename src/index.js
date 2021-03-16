5fe571c23a10ab484bd6b3c9f3dd126f7bdac60a
AnwarHossainSR <mahedianwar@gmail.com>
2021-03-16 12:54:26 +0600

    JavaScript Expressions in JSX

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+const name = 'Mahedi';
 ReactDom.render(
-      <>
-        <h2>Hello Developer !!</h2>
-        <h5>Top 5 netflix series list</h5>
-        <p>1. Dark</p>
-        <p>2. Stranger Things</p>
-        <p>3. Narcos</p>
-        <p>4. La casa de Papel</p>
-        <p>5. Peaky Blinder</p>
-      </>,
-      document.getElementById('root')
-      );
+        <>
+          <h1>My name is {name}</h1>
+          <h1>My lucky number  is {5+3}</h1>
+          <h1>Random number {Math.random()}</h1>
+        </>,document.getElementById('root'));
