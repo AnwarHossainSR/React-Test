9bd722e4c8d9aa05997d28e5bac7cbf74d4f983e
AnwarHossainSR <mahedianwar@gmail.com>
2021-03-16 20:49:00 +0600

    CSS Styling & Importing CSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-
+import './index.css';
 const name = 'Mahedi Hasan'
 ReactDOM.render(
-  <h1 contentEditable="true">My name is {name}</h1>,
+  <h1 className="heading">My name is {name}</h1>,
   document.getElementById('root')
 );
