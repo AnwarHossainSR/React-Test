b781fdb8af52a6655e5bec713955cb5f4d2de40a
AnwarHossainSR <mahedianwar@gmail.com>
2021-03-16 12:29:21 +0600

    Multiple element or return array

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-//ReactDom.render(<h2>Hello World !!</h2>,document.getElementById('root'))
-
-//ReactDom.render( /*#__PURE__*/React.createElement("h2", null, "Hello World !!"), document.getElementById('root'));
-
-var h1 = document.createElement('h1');
-h1.innerHTML='Mahedi Hasan';
-document.getElementById('root').appendChild(h1);
+ReactDom.render(
+      [
+        <h2>Hello Mahedi !!</h2>,
+        <p>How are you?</p>,
+        <p>Yah i am fine and you?</p>
+      ],
+      document.getElementById('root')
+      );
