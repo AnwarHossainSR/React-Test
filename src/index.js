c02b4d039d83e3536f3de855b66b13e2f98e12aa
AnwarHossainSR <mahedianwar@gmail.com>
2021-03-16 20:15:59 +0600

    get date time in react

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDom from 'react-dom';
 
 const fname = 'Mahedi';
 const lname = 'Hasan';
+var today = new Date(),
+date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate(),
+time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
 ReactDom.render(
         <>
           <h1>My first name is {fname} and last name is {lname}</h1>
-        </>,document.getElementById('root'));
+          <p>Today's date is : {date}</p>
+          <p>and time is : {time}</p>
+        </>,document.getElementById('root')
+        );
