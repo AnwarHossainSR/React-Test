7e45e0bce1af876d390a2fd95014a3855d60ac8b
AnwarHossainSR <mahedianwar@gmail.com>
2021-03-16 13:00:20 +0600

    ES6 Template Literals

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const name = 'Mahedi';
+const fname = 'Mahedi';
+const lname = 'Hasan';
 ReactDom.render(
         <>
-          <h1>My name is {name}</h1>
-          <h1>My lucky number  is {5+3}</h1>
-          <h1>Random number {Math.random()}</h1>
+          <h1>My first name is {fname} and last name is {lname}</h1>
         </>,document.getElementById('root'));
