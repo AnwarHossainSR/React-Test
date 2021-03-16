fe2b9b807a59f4313633d1ce73be40c793be96e7
AnwarHossainSR <mahedianwar@gmail.com>
2021-03-16 12:22:11 +0600

    Jsx

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-ReactDom.render(<h2>Hello World !!</h2>,document.getElementById('root'))
+//ReactDom.render(<h2>Hello World !!</h2>,document.getElementById('root'))
+
+//ReactDom.render( /*#__PURE__*/React.createElement("h2", null, "Hello World !!"), document.getElementById('root'));
+
+var h1 = document.createElement('h1');
+h1.innerHTML='Mahedi Hasan';
+document.getElementById('root').appendChild(h1);
