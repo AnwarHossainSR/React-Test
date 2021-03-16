18f540bb452ddb1e293e90d52ae1e7a1aacb4eb8
AnwarHossainSR <mahedianwar@gmail.com>
2021-03-16 12:44:38 +0600

    Top 5 netflix series component examle

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 ReactDom.render(
-      <React.Fragment>
-        <h2>Hello Mahedi !!</h2>,
-        <p>How are you?</p>,
-        <p>Yah i am fine and you?</p>
-      </React.Fragment>,
+      <>
+        <h2>Hello Developer !!</h2>
+        <h5>Top 5 netflix series list</h5>
+        <p>1. Dark</p>
+        <p>2. Stranger Things</p>
+        <p>3. Narcos</p>
+        <p>4. La casa de Papel</p>
+        <p>5. Peaky Blinder</p>
+      </>,
       document.getElementById('root')
       );
